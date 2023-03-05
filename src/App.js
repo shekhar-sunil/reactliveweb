d13172328f4a0716fc17226edeab11d5b070d558
shekhar-sunil <127013432+shekhar-sunil@users.noreply.github.com>
2023-03-05 11:31:05 -0800

    first commit reactwebsite

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import './Style.css';
+function App() {
+  return (
+    <div className="App">
+      <article>
+        <header>
+        <h1 id="top">Welcome to <span  className="new">wildlife organization </span><em>WFO</em></h1>
+        </header>
+        <div className="div1">
+        <ul>
+        <li><a id="active" href="#home">Home</a></li>
+        <li><a href="#About">About</a></li>
+        <li><a href="#Contact">Contact</a></li>
+        <li><a href="#Services">Services</a></li>
+        </ul>
+        </div>
+    </article>
+          </div>
+  );
+}
+
+export default App;
