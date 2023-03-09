57bc7ef54643158589b28c45d13795b0cf8b2a3b
shekharsunil <127013432+shekhar-sunil@users.noreply.github.com>
2023-03-09 10:20:43 -0800

    Update App.js

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ function App() {
         <li><a id="active" href="#home">Home</a></li>
         <li><a href="#About">About</a></li>
         <li><a href="#Contact">Contact</a></li>
-        <li><a href="#Services">Services</a></li>
+        <li><a href="#Services">Servicess</a></li>
         </ul>
         </div>
     </article>
